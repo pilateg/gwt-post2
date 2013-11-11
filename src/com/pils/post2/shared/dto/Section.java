e2e07d544ef540d19c2550610b380db4c3600c97
tzhlobitch <tzhlobitch@kyriba.com>
2013-11-11 15:48:44 +0300

    removed transient attribute

--- src/com/pils/post2/shared/dto/Section.java
+++ src/com/pils/post2/shared/dto/Section.java
@@ -6,9 +6,7 @@ public class Section extends Entity {
 	private String title;
 	private User owner;
 	private boolean openForAll;
-	transient
 	private List<User> usersWithAccess;
-	transient
 	private List<Entry> entries;
 
 	@Override
